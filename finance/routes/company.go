3539f0b3c861bb4b2da67c0911c2118a55af6997
zhangxinwei <164271701@qq.com>
2017-03-17 10:39:23 +0800

    跳转地址

--- finance/routes/company.go
+++ finance/routes/company.go
@@ -8,4 +8,22 @@ import (
 func RegCompany(rg *gin.RouterGroup) {
 	// 获取广告列表
 	rg.GET("/info", company.NewCompanyInfo().GetInfo)
+
+	/////////////////////////股东权益
+
+	//股东人数
+	rg.GET("/company/equity/shareholder", company.NewEquityInfo().GetShareholderJson)
+
+	//十大流通股东
+	rg.GET("/company/equity/top10", company.NewEquityInfo().GetTop10Json)
+
+	//机构持股
+	rg.GET("/company/equity/organization", company.NewEquityInfo().GetOrganizationJson)
+
+	//股本结构
+	rg.GET("/company/equity/structure", company.NewCapitalizationInfo().GetStructureJson)
+
+	//股本变动
+	rg.GET("/company/equity/changes", company.NewCapitalizationInfo().GetChangesJson)
+
 }
