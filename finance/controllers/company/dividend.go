5265168a9bdba62747c925a65c3b7570709ab0db
xuxiaodong <cherish_xulang@163.com>
2017-03-28 16:15:37 +0800

    分红模块结构调整

--- finance/controllers/company/dividend.go
+++ finance/controllers/company/dividend.go
@@ -1,20 +1,19 @@
 package company
 
 import (
-	//"fmt"
 	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
-	"haina.com/market/finance/models/finchina"
+	"haina.com/market/finance/models/company"
 	"haina.com/share/lib"
 )
 
 type List interface{}
 
 type Data struct {
-	Length  int         `json:"length"`
-	ComCode string      `json:"comcode"`
+	ComCode string      `json:"scode"`
+	Total   float64     `json:"tocash"`
 	List    interface{} `json:"list"`
 }
 
@@ -26,131 +25,49 @@ func NewDividendInfo() *DividendInfo {
 }
 
 func (this *DividendInfo) GetDiv(c *gin.Context) {
-	scode := strings.Split(c.Query("scode"), ".")[0]
+	scode := c.Query("scode")
 	sets, e := strconv.Atoi(c.Query("sets"))
 	if e != nil {
 		lib.WriteString(c, 40004, "invalid sets..")
 		return
 	}
-	divs, err := new(finchina.Dividend).GetDivList(uint64(sets), scode)
+	fin := new(company.FinDivid)
+	divs, err := fin.GetDivListJson(uint64(sets), strings.Split(scode, ".")[0])
 	if err != nil {
 		lib.WriteString(c, 300, err.Error())
 		return
 	}
-	data := getDivListjson(divs)
+	var data Data
+	data.Total = fin.ToCash
+	data.List = divs
 	data.ComCode = scode
-
 	lib.WriteString(c, 200, data)
 }
+
 func (this *DividendInfo) GetSEO(c *gin.Context) {
-	scode := strings.Split(c.Query("scode"), ".")[0]
-	seos, err := new(finchina.Dividend).GetSEOList(scode)
+	scode := c.Query("scode")
+	fin := new(company.FinDivid)
+	seos, err := fin.GetSEOListJson(strings.Split(scode, ".")[0])
 	if err != nil {
 		lib.WriteString(c, 300, err.Error())
+		return
 	}
-	data := getSEOListjson(seos)
+	var data Data
+	data.Total = fin.SeoRaise
+	data.List = seos
 	data.ComCode = scode
 	lib.WriteString(c, 200, data)
 }
 func (this *DividendInfo) GetRO(c *gin.Context) {
-	scode := strings.Split(c.Query("scode"), ".")[0]
-	ros, err := new(finchina.Dividend).GetROList(scode)
+	scode := c.Query("scode")
+	fin := new(company.FinDivid)
+	ros, err := fin.GetROListJson(strings.Split(scode, ".")[0])
 	if err != nil {
 		lib.WriteString(c, 300, err.Error())
 	}
-	data := getROListjson(ros)
+	var data Data
+	data.Total = fin.RoRaise
+	data.List = ros
 	data.ComCode = scode
 	lib.WriteString(c, 200, data)
-}
-
-func getDivListjson(divs []finchina.Div) Data {
-	var i int = 0
-	var data Data
-	jsn := make([]finchina.DivJson, 0)
-
-	for _, v := range divs {
-		var js finchina.DivJson
-		js.Bene = v.GRAOBJ.String
-		js.Bonus = v.PROBONUSRT.Float64
-		js.Date = v.DIVIYEAR.String
-		js.Dividend = v.PRETAXCASHMAXDVCNY.Float64
-		js.DivDate = v.CASHDVARRBEGDATE.String
-		js.INDate = v.PUBLISHDATE.String
-		js.DNDate = v.SHHDMEETRESPUBDATE.String
-		js.ExDate = v.XDRDATE.String
-		js.RegDate = v.EQURECORDDATE.String
-		js.Tran = v.TRANADDRT.Float64
-
-		if strings.EqualFold(v.LISTDATE.String, "19000101") {
-			js.LisDate = "--"
-		} else {
-			js.LisDate = v.LISTDATE.String
-		}
-
-		jsn = append(jsn, js)
-		i++
-	}
-	data.Length = i
-	data.List = jsn
-	return data
-}
-
-func getSEOListjson(seos []finchina.SEO) Data {
-	var i int = 0
-	var data Data
-	jsn := make([]*finchina.SEOJson, 0)
-
-	for _, v := range seos {
-		js := &finchina.SEOJson{
-			IECD:    v.CSRCAPPRAGREEDATE.String,
-			LisDate: v.UPDATEDATE.String,
-			//PNDate:  v.ADDISSPUBDATE.String,
-			PPrice: v.ENQUMAXPRICE.Float64,
-			Price:  v.ISSPRICE.Float64,
-			PVal:   v.PLANTOTRAISEAMT.Float64,
-			PVol:   v.PLANISSMAXQTY.Float64,
-			Range:  v.ISSUEOBJECT.String,
-			SEOD:   v.SHARECORDDATE.String,
-			SRCD:   v.CSRCAPPDPUBDATE.String,
-			Val:    v.NEWTOTRAISEAMT.Float64,
-			Vol:    v.ACTISSQTY.Float64,
-			Type:   v.ISSUEMODEMEMO.String,
-		}
-		jsn = append(jsn, js)
-		i++
-	}
-	data.Length = i
-	data.List = jsn
-	return data
-}
-
-func getROListjson(ros []finchina.RO) Data {
-	var i int = 0
-	var data Data
-	jsn := make([]finchina.ROJson, 0)
-
-	for _, v := range ros {
-		var js finchina.ROJson
-		js.AGMD = v.UPDATEDATE.String
-		js.Code = v.ALLOTCODE.String
-		js.DNDate = v.LISTPUBDATE.String
-		js.ERDate = v.EXRIGHTDATE.String
-		js.LisDate = v.LISTDATE.String
-		js.PNDate = v.PUBLISHDATE.String
-		js.PProp = v.ACTTOTALLOTRT.Float64
-		js.Price = v.ALLOTPRICE.Float64
-		js.Prop = v.ALLOTRT.Float64
-		js.PVol = v.PLANISSMAXQTY.Float64
-		js.RegDate = v.EQURECORDDATE.String
-		js.ROPD = v.PAYBEGDATE.String + "~" + v.PAYENDDATE.String
-		js.Short = v.ALLOTSNAME.String
-		js.Vol = v.ACTISSQTY.Float64
-
-		jsn = append(jsn, js)
-		i++
-	}
-	data.Length = i
-	data.List = jsn
-
-	return data
 }
