9b6f67569c0a5c22e9ebfbded228338a4a1ea218
Yuan Hong <yuanhack@163.com>
2017-04-06 09:32:57 +0800

    修改 finchina getCompcode

--- finance/models/finchina/financial_analysis.go
+++ finance/models/finchina/financial_analysis.go
@@ -7,6 +7,7 @@ import (
 
 	. "haina.com/share/models"
 
+	redigo "haina.com/share/garyburd/redigo/redis"
 	"haina.com/share/gocraft/dbr"
 	"haina.com/share/logging"
 	"haina.com/share/store/redis"
@@ -31,18 +32,23 @@ func (this *SymbolToCompcode) getCompcode(symbol string) error {
 	key := fmt.Sprintf(REDIS_SYMBOL_COMPCODE, symbol)
 	v, err := redis.Get(key)
 	if err != nil {
-		logging.Error("Get %s: %s", key, err)
+		if err != redigo.ErrNil {
+			logging.Error("Redis get %s: %s", key, err)
+		}
 		err := this.Db.Select("COMPCODE").From(this.TableName).Where("SYMBOL=?", symbol).Limit(1).LoadStruct(this)
 		if err != nil {
+			logging.Error("finchina db: getCompcode: %s", err)
 			return err
 		}
 		if this.COMPCODE.Valid == false {
-			logging.Error("finchina db: select COMPCODE from %s where SYMBOL='%s'", TABLE_TQ_OA_STCODE, symbol)
+			logging.Error("finchina db: getCompcode: select COMPCODE from %s where SYMBOL='%s'", TABLE_TQ_OA_STCODE, symbol)
 			return ErrNullComp
 		}
 		if err := redis.Setex(key, REDIS_TTL, []byte(this.COMPCODE.String)); err != nil {
+			logging.Error("Redis cache %s TTL %d: %s", key, REDIS_TTL, err)
 			return err
 		}
+		logging.Info("Redis cache %s TTL %d", key, REDIS_TTL)
 		return nil
 	}
 
