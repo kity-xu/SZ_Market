f2a578dd3e075d440eb9bcb80015bc4873fb5aad
zhangxinwei <164271701@qq.com>
2017-04-21 09:25:05 +0800

    提交daykline文件

---
+++ hqpublish/controllers/publish/daykline.go
@@ -0,0 +1,34 @@
+package publish
+
+import (
+	"github.com/gin-gonic/gin"
+	"haina.com/market/hqpublish/models"
+	redigo "haina.com/share/garyburd/redigo/redis"
+	"haina.com/share/lib"
+	"haina.com/share/logging"
+	"haina.com/share/store/redis"
+)
+
+type DayKLine struct{}
+
+func NewDayKLine() *DayKLine {
+	return &DayKLine{}
+}
+
+// 获取日K先历史数据
+func (this *DayKLine) GET(c *gin.Context) {
+	snid := c.Query(models.CONTEXT_SNID)
+	key := "kline:day:" + snid
+	klpbinfo, err := redigo.Bytes(redis.Get(key))
+	if err != nil {
+		// 没找到
+		if err == redigo.ErrNil {
+			logging.Info("没找到数据 %v", err)
+			return
+		}
+		// 其他错误
+		return
+	}
+
+	lib.WriteData(c, klpbinfo)
+}
