86693971fa3d3e4f62203996508aad7641bfa251
zhangxinwei <164271701@qq.com>
2017-05-03 09:49:31 +0800

    删除dkline

--- hqpublish/routes/publish.go
+++ hqpublish/routes/publish.go
@@ -10,9 +10,6 @@ func RegPublish(rg *gin.RouterGroup) {
 	// 股票代码表
 	rg.GET("/securitytable", publish.NewSecurityTable().GET) //默认pb模式
 
-	// 日K线历史数据
-	rg.GET("/kline/dayk", publish.NewDayKLine().GET) //默认pb模式
-
 	// 分钟K线
 	rg.POST("/min", publish.NewMinKLine().POST)
 
