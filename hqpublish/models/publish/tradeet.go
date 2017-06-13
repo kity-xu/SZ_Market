8d5fef859575530771ec0a394a5435195eb61f36
Yuan Hong <yuanhack@163.com>
2017-06-13 09:34:00 +0800

    读写锁版实现同步成交记录Store->Cache(redis list)

--- hqpublish/models/publish/tradeet.go
+++ hqpublish/models/publish/tradeet.go
@@ -18,6 +18,7 @@ import (
 
 	hsgrr "haina.com/share/garyburd/redigo/redis"
 	"haina.com/share/logging"
+	"haina.com/share/store/redis"
 )
 
 var (
@@ -43,61 +44,75 @@ func NewTradeEveryTime() *TradeEveryTime {
 	}
 }
 
-var tradeet sync.Mutex
+var TradeCacheKey = REDISKEY_SECURITY_TRADE
 
-func LockTradeet() {
+//------------------------------------------------------------------------------
+// 读写锁版实现
+// RedisCache 缓存成交记录list(和RedisStore一致)
+type TradeetSentinel struct {
+	Timestamp int64         // 最后同步时间戳
+	SyncFlag  bool          // true/false:正在同步/完成同步
+	RWLock    *sync.RWMutex // 读写锁
+}
+
+//------------------------------------------------------------------------------
+var (
+	tradeetMap map[int32]*TradeetSentinel = make(map[int32]*TradeetSentinel, 5000)
+	tradeet    sync.Mutex
+)
+
+func LockTradeet() { // for tradeetMap
 	tradeet.Lock()
 }
 func UnlockTradeet() {
 	tradeet.Unlock()
 }
 
-type TradeetSentinel struct {
-	SyncFlag  bool
-	Timestamp int64
-	RWLock    *sync.RWMutex
-}
-
+//------------------------------------------------------------------------------
 func NewTradeetSentinel() *TradeetSentinel {
 	return &TradeetSentinel{
+		SyncFlag:  false,
 		Timestamp: time.Now().Unix(),
 		RWLock:    &sync.RWMutex{},
 	}
 }
-func getNowTradeetSentinel(sid int32) *TradeetSentinel {
+
+func UseTradeetSentinel(sid int32) *TradeetSentinel {
 	LockTradeet()
 	defer UnlockTradeet()
 
-	t, ok := TradeetMap[sid]
+	t, ok := tradeetMap[sid]
 	if ok {
-		fmt.Printf("get trade every time sentinel %x\n", t)
+		fmt.Printf("get trade every time sentinel %+v\n", t)
 		return t
 	}
 
-	fmt.Printf("new trade every time sentinel %x\n", t)
+	fmt.Printf("new trade every time sentinel %+v\n", t)
 	t = NewTradeetSentinel()
-	TradeetMap[sid] = t
+	tradeetMap[sid] = t
 
 	return t
 }
 
-var TradeetMap map[int32]*TradeetSentinel = make(map[int32]*TradeetSentinel, 5000)
+//------------------------------------------------------------------------------
 
+//------------------------------------------------------------------------------
 // 将分笔成交数据同步到缓存
 // 返回值 int
-//   >0 本次同步到缓存的条数
-//   =0 缓存和数据redis一致，本次没有进行同步
-//   -1 redis缓存错误不可用
-//   -2 redis数据错误，没有数据
-func (this TradeEveryTime) SyncTradeEveryTimeRecord(sid int32) (int, error) {
-	key := fmt.Sprintf(this.CacheKey, sid)
+//   -1  错误，缓存redis不可用(本次后续操作跳过缓存redis)
+//   -2  错误，数据redis不可用或没有数据(返回客户端 code 40002)
+//   >=0 缓存key-list长度(llen key)，目前缓存redis和数据redis已同步一致
+func SyncTradeEveryTimeRecord(sid int32) (int, error) {
+	key := fmt.Sprintf(TradeCacheKey, sid)
 
-	t := getNowTradeetSentinel(sid)
-
+	t := UseTradeetSentinel(sid)
 	t.RWLock.RLock()
-	defer t.RWLock.RUnlock()
-
-	t.SyncFlag = false
+	if t.SyncFlag {
+		t.RWLock.RUnlock()
+		fmt.Printf("%d In the synchronous\n", sid)
+		return 0, nil
+	}
+	t.RWLock.RUnlock()
 
 	clen, err := RedisCache.Llen(key) // 出错时返回值 clen = -1
 	if err != nil {
@@ -117,29 +132,29 @@ func (this TradeEveryTime) SyncTradeEveryTimeRecord(sid int32) (int, error) {
 	fmt.Printf("%s Store len %d, Cache len %d\n", key, slen, clen)
 
 	if slen == clen {
-		return 0, nil
+		return slen, nil
 	} else if slen < clen {
 		RedisCache.Del(key)
 	}
 
 	ls, err := RedisStore.LRange(key, clen, slen-1)
 
-	go func() {
-		t.RWLock.Lock()
-		defer t.RWLock.Unlock()
-
-		t.SyncFlag = true
+	t.RWLock.Lock()
+	t.SyncFlag = true
+	t.RWLock.Unlock()
+	{
 		for _, v := range ls {
 			RedisCache.Rpush(key, []byte(v))
 		}
 		logging.Info("sid %d trade Sync TradeEveryTimeRecord done", sid)
-		fmt.Printf("func %s Store len %d, Cache len %d\n", key, slen, clen)
-		t.Timestamp = time.Now().Unix()
-		t.SyncFlag = false
-	}()
 
-	fmt.Printf("---- %s Store len %d, Cache len %d\n", key, slen, clen)
-	return slen - clen, nil
+		t.Timestamp = time.Now().Unix()
+	}
+	t.RWLock.Lock()
+	t.SyncFlag = false
+	t.RWLock.Unlock()
+
+	return slen, nil
 }
 
 func (this TradeEveryTime) GetTradeEveryTimeJson(req *pro.RequestTradeEveryTime) ([]byte, error) {
@@ -156,24 +171,37 @@ func (this TradeEveryTime) GetTradeEveryTimePB(req *pro.RequestTradeEveryTime) (
 	}
 	return ctrl.MakeRespDataByPB(200, pro.HAINA_PUBLISH_CMD_ACK_TRADEET, payload)
 }
-
 func (this TradeEveryTime) GetTradeEveryTimeObj(req *pro.RequestTradeEveryTime) (*pro.PayloadTradeEveryTime, error) {
+
+	go SyncTradeEveryTimeRecord(req.SID)
+
+	t := UseTradeetSentinel(req.SID)
+	t.RWLock.RLock()
+	defer t.RWLock.RUnlock()
+
+	if t.SyncFlag {
+		return this.GetPayloadTradeEveryTimeObj(RedisStore, req)
+	}
+
+	payload, err := this.GetPayloadTradeEveryTimeObj(RedisCache, req)
+	if err == nil {
+		return payload, nil
+	}
+
+	return this.GetPayloadTradeEveryTimeObj(RedisStore, req)
+}
+
+func (this TradeEveryTime) GetTradeEveryTimeRecordList(rds *redis.RedisPool, req *pro.RequestTradeEveryTime) ([]*pro.TradeEveryTimeRecord, int, error) {
 
 	key := fmt.Sprintf(this.CacheKey, req.SID)
 
-	slen, err := RedisStore.Llen(key)
+	slen, err := rds.Llen(key)
 	if err != nil {
 		logging.Error("%v", err)
-		return nil, err
+		return nil, -1, err
 	}
 	if slen == 0 {
-		return &pro.PayloadTradeEveryTime{
-			SID:     req.SID,
-			Total:   int32(slen),
-			Begin:   req.Begin,
-			Num:     0,
-			DTRList: nil,
-		}, nil
+		return nil, 0, hsgrr.ErrNil
 	}
 
 	bgn, end := 0, -1
@@ -192,10 +220,10 @@ func (this TradeEveryTime) GetTradeEveryTimeObj(req *pro.RequestTradeEveryTime) 
 
 	logging.Info("tradeet %d list range[%d,%d]", req.SID, bgn, end)
 
-	ls, err := RedisStore.LRange(key, bgn, end)
+	ls, err := rds.LRange(key, bgn, end)
 	if err != nil {
 		logging.Error("%v", err)
-		return nil, err
+		return nil, slen, err
 	}
 
 	rows := make([]*pro.TradeEveryTimeRecord, 0, 5000)
@@ -205,18 +233,27 @@ func (this TradeEveryTime) GetTradeEveryTimeObj(req *pro.RequestTradeEveryTime) 
 		bufer := bytes.NewBuffer([]byte(v))
 		if err := binary.Read(bufer, binary.LittleEndian, trade); err != nil && err != io.EOF {
 			logging.Error("%v", err)
-			return nil, err
+			return nil, slen, err
 		}
 		rows = append(rows, trade)
 	}
 	logging.Info("tradeet %d get list range[%d,%d] trade data done.", req.SID, bgn, end)
 
+	return rows, slen, nil
+}
+
+func (this TradeEveryTime) GetPayloadTradeEveryTimeObj(rds *redis.RedisPool, req *pro.RequestTradeEveryTime) (*pro.PayloadTradeEveryTime, error) {
+
+	rows, total, err := this.GetTradeEveryTimeRecordList(rds, req)
+	if err != nil {
+		return nil, err
+	}
+
 	return &pro.PayloadTradeEveryTime{
 		SID:     req.SID,
-		Total:   int32(slen),
+		Total:   int32(total),
 		Begin:   req.Begin,
 		Num:     int32(len(rows)),
 		DTRList: rows,
 	}, nil
-
 }
