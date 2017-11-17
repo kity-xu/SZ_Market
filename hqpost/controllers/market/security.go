9e3db9bd056dcb048ee6f5c291e1d3a40585ecf0
xuxiaodong <cherish_xulang@163.com>
2017-11-17 13:21:38 +0800

    hqpost 开发完成

--- hqpost/controllers/market/security.go
+++ hqpost/controllers/market/security.go
@@ -3,14 +3,14 @@ package market
 import (
 	"time"
 
-	"haina.com/market/hqpost/controllers/market/kline"
-	"haina.com/market/hqpost/controllers/market/sidcode"
-
 	. "haina.com/market/hqpost/controllers"
-	"haina.com/market/hqpost/controllers/market/bline"
-	"haina.com/market/hqpost/controllers/market/minline"
+
+	"os"
 
 	"haina.com/market/hqpost/config"
+	"haina.com/market/hqpost/controllers/market/kline"
+	"haina.com/market/hqpost/controllers/market/minline"
+	"haina.com/market/hqpost/controllers/market/sidcode"
 	"haina.com/market/hqpost/models/redistore"
 	"haina.com/share/logging"
 )
@@ -27,26 +27,84 @@ func Update(cfg *config.AppConfig) {
 
 	//---------------------日周月年线---------------------/
 	//K线
-	kline.InitPath(cfg)
-	kline.HisDayKline(sids)
-	kline.HisWeekKline(sids)
-	kline.HisMonthKline(sids)
-	kline.HisYearKline(sids)
-	//--------------------------------------------------/
+	if len(os.Args) == 3 {
+		min := minline.NewMinKline(sids, cfg)
+		kline.InitPath(cfg)
 
-	//----------------------分钟线-----------------------/
-	min := minline.NewMinKline(sids, cfg)
-	min.InitMinLine()
-	// 分钟线
-	min.HMinLine_1()
-	min.HMinLine_5()
-	min.HMinLine_15()
-	min.HMinLine_30()
-	min.HMinLine_60()
-	//--------------------------------------------------/
+		switch os.Args[2] {
+		case "-m":
+			logging.Info("--------min----------")
+			min.InitMinLine()
+			// 分钟线
+			min.HMinLine_1()
+			min.HMinLine_5()
+			min.HMinLine_15()
+			min.HMinLine_30()
+			min.HMinLine_60()
+		case "-o":
+			logging.Info("--------oDay----------")
+			kline.HisDayKline(sids)
+			kline.HisWeekKline(sids)
+			kline.HisMonthKline(sids)
+			kline.HisYearKline(sids)
+		case "-day":
+			logging.Info("--------day----------")
+			kline.HisDayKline(sids)
+		case "-week":
+			logging.Info("--------week----------")
+			kline.HisWeekKline(sids)
+		case "-month":
+			logging.Info("--------month----------")
+			kline.HisMonthKline(sids)
+		case "-year":
+			logging.Info("--------year----------")
+			kline.HisYearKline(sids)
+		case "-m01":
+			logging.Info("--------min01----------")
+			min.InitMinLine()
+			min.HMinLine_1()
+		case "-m05":
+			logging.Info("--------min05----------")
+			min.InitMinLine()
+			min.HMinLine_5()
+		case "-m15":
+			logging.Info("--------min15----------")
+			min.InitMinLine()
+			min.HMinLine_15()
+		case "-m30":
+			logging.Info("--------min30----------")
+			min.InitMinLine()
+			min.HMinLine_30()
+		case "-m60":
+			logging.Info("--------min60----------")
+			min.InitMinLine()
+			min.HMinLine_60()
+		default:
+			return
+		}
 
+	} else {
+		logging.Info("--------all----------")
+		kline.InitPath(cfg)
+		kline.HisDayKline(sids)
+		kline.HisWeekKline(sids)
+		kline.HisMonthKline(sids)
+		kline.HisYearKline(sids)
+		//--------------------------------------------------/
+
+		//----------------------分钟线-----------------------/
+		min := minline.NewMinKline(sids, cfg)
+		min.InitMinLine()
+		// 分钟线
+		min.HMinLine_1()
+		min.HMinLine_5()
+		min.HMinLine_15()
+		min.HMinLine_30()
+		min.HMinLine_60()
+		//--------------------------------------------------/
+	}
 	//----------------------板块指数线----------------------/
-	bline.NewBlockIndex().UpdateBlockIndexDayLine(cfg)
+	//bline.NewBlockIndex().UpdateBlockIndexDayLine(cfg)
 	//--------------------------------------------------/
 
 	end := time.Now()
